5876864c1c5412c2447bb6577c54db9206f323e6
chomczykdavid <82461908+chomczykdavid@users.noreply.github.com>
2021-04-15 23:56:08 -0300

    Add files via upload
    archivo nuevo

---
+++ tp-laboratorio1/src/tp-laboratorio1.c
@@ -0,0 +1,33 @@
+/*
+ ============================================================================
+ Name        : tp-laboratorio1.c
+ Author      : da
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "operaciones.h"
+
+int main()
+{
+
+	setbuf(stdout, NULL);
+
+    menu();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
