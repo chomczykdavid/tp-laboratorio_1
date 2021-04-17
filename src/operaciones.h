f9b679570a88e75597a075da73dd75ce29551e4f
chomczykdavid <82461908+chomczykdavid@users.noreply.github.com>
2021-04-16 21:30:52 -0300

    Add files via upload
    nuevos cambios

---
+++ src/operaciones.h
@@ -0,0 +1,29 @@
+/*
+ * operaciones.h
+ *
+ *  Created on: 15 abr. 2021
+ *      Author: 54113
+ */
+#include <stdio.h>
+#ifndef OPERACIONES_H_
+#define OPERACIONES_H_
+
+
+/**
+ * @fn int sumar(int, int)
+ * @brief
+ *
+ * @param operador1
+ * @param operador2
+ * @return
+ */
+
+// prototipo de la funcion
+int sumar(int operador1, int operador2);
+int restar(int opeador1, int operador2);
+int multiplicar(int opeador1, int operador2);
+float dividir(int opeador1, int operador2);
+int factorial(int operador1);
+void menu();
+
+#endif /* OPERACIONES_H_ */
